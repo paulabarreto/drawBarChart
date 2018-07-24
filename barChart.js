22c3c1ddbfd6a09891a6c060437373a237d0eb58
Paula <37316520+paulabarreto@users.noreply.github.com>
2018-07-24 14:26:19 -0400

    Bar chart - first styling

--- barChart.js
+++ barChart.js
@@ -1,8 +1,8 @@
-function drawBarChart(data) {
+function drawBarChart(data, width, height) {
   data.sort(function(a, b){return b - a});
   for (var i = 0; i < data.length; i++) {
     document.write("<tr>");
-    document.write("<td>"+data[i]+"</td>");
+    document.write("<th>"+data[i]+"</th>");
     for(var j = 0; j < data[i]; j++){
       document.write("<td>"+"|"+"</td>");
     }
