e51ff751706b34d1b6b3af9191e7274c1145da1f
Paula <37316520+paulabarreto@users.noreply.github.com>
2018-07-24 12:36:03 -0400

    Bar chart - Just Data

--- barChart.js
+++ barChart.js
@@ -1,24 +1,12 @@
 function drawBarChart(data) {
-  var graph = [];
-  for (var i = data.length; i > 0; i--) {
+  data.sort(function(a, b){return b - a});
+  for (var i = 0; i < data.length; i++) {
     document.write("<tr>");
-    document.write("<td>"+data[i - 1]+"</td>");
-    if(data[i-1] === data[0]){
-      document.write("<td>"+"|"+"</td>");
-      document.write("<td>"+"|"+"</td>");
-      document.write("<td>"+"|"+"</td>");
-    }
-    if(data[i-1] === data[1]) {
-      document.write("<td>"+"..."+"</td>");
-      document.write("<td>"+"|"+"</td>");
-      document.write("<td>"+"|"+"</td>");
-    }
-    if(data[i-1] === data[2]) {
-      document.write("<td>"+"..."+"</td>");
-      document.write("<td>"+"..."+"</td>");
+    document.write("<td>"+data[i]+"</td>");
+    for(var j = 0; j < data[i]; j++){
       document.write("<td>"+"|"+"</td>");
     }
     document.write("</tr>");
   }
-  return graph;
+  return null;
 }
