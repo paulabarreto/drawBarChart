1e28ca8b7b3d246ca1729e55c96aa2fba22400f7
Paula <37316520+paulabarreto@users.noreply.github.com>
2018-07-27 11:42:02 -0400

    Bar chart is ready, needs styling

--- barChart.js
+++ barChart.js
@@ -1,22 +1,22 @@
 function drawBarChart(data, width, height) {
   data.sort(function(a, b){return b - a});
   for (var i = 0; i < data.length; i++) {
-    document.write("<tr>");
+    document.write("<tr>");                       //Number Column
     for (var k = 0; k < data[i]; k++){
       if(k === 0 && i === 0){
-        document.write("<td>"+"&nbsp"+"</td>");
+        document.write("<td id=inter>"+"&nbsp"+"</td>");   //1st column + 1st line intersection
       }
       if(i === 0){
-        document.write("<th>"+(k + 1)+"</th>");
+        document.write("<th>"+(k + 1)+"</th>");   //Numbers inside first column
       }
     }
 
-    document.write("<tr>");
-    document.write("<th>"+data[i]+"</th>");
+    document.write("<tr>");                        //Next column
+    document.write("<th>"+data[i]+"</th>");       //Tags
     for(var j = 0; j < data[i]; j++){
-      document.write("<td>"+"&nbsp"+"</td>");
+      document.write("<td>"+"&nbsp"+"</td>");     //Bars
     }
-    document.write("</tr>");
+    document.write("</tr>");                      //Next column
   }
   return null;
 }
