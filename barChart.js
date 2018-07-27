7a6a3e235c0558d5990443be3d535f640e6eae1f
Paula <37316520+paulabarreto@users.noreply.github.com>
2018-07-27 11:09:36 -0400

    Vertical Bar Chart

--- barChart.js
+++ barChart.js
@@ -2,6 +2,16 @@ function drawBarChart(data, width, height) {
   data.sort(function(a, b){return b - a});
   for (var i = 0; i < data.length; i++) {
     document.write("<tr>");
+    for (var k = 0; k < data[i]; k++){
+      if(k === 0 && i === 0){
+        document.write("<td>"+"&nbsp"+"</td>");
+      }
+      if(i === 0){
+        document.write("<th>"+(k + 1)+"</th>");
+      }
+    }
+
+    document.write("<tr>");
     document.write("<th>"+data[i]+"</th>");
     for(var j = 0; j < data[i]; j++){
       document.write("<td>"+"&nbsp"+"</td>");
