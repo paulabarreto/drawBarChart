55a5fbdf65b6416fac799191c7bdbbd8e95cfd1c
Paula <37316520+paulabarreto@users.noreply.github.com>
2018-08-01 13:04:31 -0400

    User can choose color of bars
    Next step: customize each bar instead of all at the same time

--- barChart.js
+++ barChart.js
@@ -5,43 +5,55 @@ $(document).ready(function(){
   }
   $("#valueQty").html(select);
 
-$(document).on("input", "#valueQty", function() {
-  var valueEntered = false;
-  if ($(this).val() !== "") {
-        valueEntered = true;
-        var valueQty = $(this).val();
-        for(var j = 0; j < valueQty; j++){
-          var inputValue = document.createElement("input");
-          inputValue.setAttribute("type", "text");
-          inputValue.setAttribute("id", "value" + j);
-          $("#inputVal").append(inputValue);
-        }
-  }
-});
-
-$("button").click(function(){
-  var data = [];
-  for(var k = 0; k < 50; k++){
-    if($("#value" + k).val() !== undefined){
-      data.push($("#value" + k).val());
+  $(document).on("input", "#valueQty", function() {
+    var valueEntered = false;
+    if ($(this).val() !== "") {
+      valueEntered = true;
+      var valueQty = $(this).val();
+      for(var j = 0; j < valueQty; j++){
+        var inputValue = document.createElement("input");
+        inputValue.setAttribute("type", "text");
+        inputValue.setAttribute("id", "value" + j);
+        $("#inputVal").append(inputValue);
+      }
     }
-  }
-  var barChart = drawBarChart($("#barChart"), data);
-});
-function drawBarChart(container, data) {
-  var table = $("<table />");
-  $.each(data, function(index, value) {
-    var row = $("<tr />");
-    row.append("<th>"+value+"</th>");
-    for(var i = 0; i < value; i++){
-      var col = $("<td />");
-      row.append(col);
-      table.append(row);
-    }
-    row.append("<th>"+value+"</th>");
-    table.append(row);
   });
-  return container.append(table);
-}
+
+  $("button").click(function(){
+    var data = [];
+    for(var k = 0; k < 50; k++){
+      if($("#value" + k).val() !== undefined){
+        data.push($("#value" + k).val());
+      }
+    }
+    var barChart = drawBarChart($("#barChart"), data);
+  });
+  function drawBarChart(container, data) {
+    var table = $("<table />");
+    $.each(data, function(index, value) {
+      var row = $("<tr />");
+      row.append("<th>"+value+"</th>");
+      for(var i = 0; i < value; i++){
+        var col = $("<td />");
+        row.append(col);
+        table.append(row);
+      }
+      row.append("<th>"+value+"</th>");
+      table.append(row);
+      var colorWell;
+      document.addEventListener("click", startup, false);
+      function startup(){
+        colorWell = document.querySelector("#colorWell");
+        colorWell.addEventListener("input", updateFirst, false);
+        colorWell.select();
+      }
+      function updateFirst(event){
+        document.querySelectorAll("td").forEach(function(p){
+          p.style.backgroundColor = event.target.value;
+        });
+      }
+    });
+    return container.append(table);
+    }
 
 });
