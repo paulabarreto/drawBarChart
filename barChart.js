15ca30d2fcb1735987031f927228417224626804
Paula <37316520+paulabarreto@users.noreply.github.com>
2018-08-14 14:52:20 -0400

    Title colour customizable

--- barChart.js
+++ barChart.js
@@ -124,9 +124,14 @@ $(document).ready(function(){
   function drawBarChart(data, options, element) {
 
     var table = $("<table id=table />");
+
     $(table).height(options.height);
     $(table).width(options.width);
 
+
+    var title = $("<h2>&emsp;&emsp;&emsp;"+options.title+"</h2>");
+    title.addClass("chartTitle");
+    element.append(title);
 
     $.each(data, function(index, value) {
 
@@ -134,57 +139,102 @@ $(document).ready(function(){
         return o.Value;
       }))
 
-      //Y-Axis
-      if(index === 0){
-        var yAxis = $("<tr class=yAxis/>");
 
-        for(j = 0; j <= maxValue; j++){
-          if(maxValue > 0 && maxValue <= 10){
-            yAxis.append("<td class=yAxis> <p>"+j+"____</p></td>");
-          } else if (maxValue > 10 && maxValue <= 20){
-            if(j === 0 || j % 5 === 0){
-              yAxis.append("<td class=yAxis> <p>"+j+"___</p></td>");
-            } else{
-            yAxis.append("<td class=yAxis> <p>&nbsp</p></td>");
+      //Y-AXIS
+
+      var gap = 1;
+
+      if (maxValue <= 10){
+        gap = 2;
+      } else if(maxValue > 10 && maxValue <= 100){
+        gap = 3;
+      } else if(maxValue > 100 && maxValue <= 500){
+        gap = 4;
+      }
+
+      var row = $("<tr />");
+
+      if(index === 0){
+
+        var yAxis = $("<tr />");
+
+        for(var j = 0; j <= maxValue; j++){
+
+          switch(gap){
+
+            case 1:
+            var yAxisTd = $("<td class=yAxis> <p class=yAxis>"+j+"</p></td>");
+            yAxis.append(yAxisTd);
+            break;
+
+            case 2:
+            if (j % 2 === 0){
+              var yAxisTd = $("<td class=yAxis> <p class=yAxis>"+j+"</p></td>");
+              yAxis.append(yAxisTd);
             }
-          } else if(maxValue > 20 && maxValue <= 50){
-            if(j === 0 || j % 10 === 0){
-              yAxis.append("<td class=yAxis> <p>"+j+"___</p></td>");
-            } else{
-              yAxis.append("<td class=yAxis> <p>&nbsp</p></td>");
+            else {
+              var yAxisTd = $("<td class=yAxisN/>");
+              yAxis.append(yAxisTd);
             }
-          } else if(maxValue > 50 && maxValue <= 100){
-            if(j === 0 || j % 20 === 0){
-              yAxis.append("<td class=yAxis> <p>"+j+"___</p></td>");
-            } else{
-              yAxis.append("<td class=yAxis> <p>&nbsp</p></td>");
+            break;
+
+            case 3:
+            if (j % 10 === 0){
+              var yAxisTd = $("<td class=yAxis> <p class=yAxis>"+j+"</p></td>");
+              yAxis.append(yAxisTd);
+            }
+            else {
+              var yAxisTd = $("<td class=yAxisN/>");
+              yAxis.append(yAxisTd);
+            }
+            break;
+
+          case 4:
+          if (j % 100 === 0){
+              var yAxisTd = $("<td class=yAxis> <p class=yAxis>"+j+"</p></td>");
+              yAxis.append(yAxisTd);
+            }
+            else{
+              var yAxisTd = $("<td class=yAxisN/>");
+              yAxis.append(yAxisTd);
             }
           }
 
-          table.append(yAxis);
+
         }
+
+        table.append(yAxis);
+
       }
 
       //X-Axis
+
       var row = $("<tr />");
-      row.append("<th> <p class=label" + index+">"+this.Label+"</p></th>");
+
+      row.append("<th> <p class=label"+index+">"+this.Label+"</p></th>");
+
       //Bars
-      for(var i = 0; i < (this.Value - 1); i++) {
-        if(i === 0 && options.position === "3"){
+      for(var i = 0; i < this.Value; i++) {
+
+          if(i === 0 && options.position === "3"){
           var col = $("<td> <p class=number>"+this.Value+"</p></td>");
-        } else if(i === (this.Value - 2) && options.position === "1"){
-          var col = $("<td> <p class=number>"+this.Value+"</p></td>");
+        } else if(i === (this.Value - 1) && options.position === "1"){
+          var col = $("<td> <p class=number id=cell"+i+">"+this.Value+"</p></td>");
         } else if((i === ((this.Value - 1)/2) || i === (this.Value / 2)) && options.position === "2"){
           var col = $("<td> <p class=number>"+this.Value+"</p></td>");
         } else {
           var col = $("<td />");
         }
 
+        if(i === 0){
+          (col).addClass("xAxis");
+        }
+
         (col).addClass("bar" + index);
         row.append(col);
         table.append(row);
+    }
 
-      }
 
 
 
@@ -195,7 +245,6 @@ $(document).ready(function(){
       }
 
 
-
       /*Color customization*/
 
       //BARS
@@ -203,27 +252,25 @@ $(document).ready(function(){
       colorWell.setAttribute("type", "color");
       colorWell.setAttribute("id", "colorWell" + index);
       document.addEventListener("click", startup, false);
+
       function startup(){
         colorWell = document.querySelector("#colorWell" + index);
         colorWell.addEventListener("input", updateAll, false);
         colorWell.select();
       }
+
       function updateAll(event) {
         document.querySelectorAll(".bar" + index).forEach(function(p) {
           p.style.backgroundColor = event.target.value;
         });
       }
 
-      //Labels for Bar Color Picker
-      var colorLabel = document.createElement("label");
-      labelLabel.setAttribute("for", label);
-      var nameLabel = document.createTextNode(this.Label);
-
       //Labels
       var colorWellLabels = document.createElement("input");
       colorWellLabels.setAttribute("type", "color");
       colorWellLabels.setAttribute("id", "colorWellLabels" + index);
       document.addEventListener("click", startupLabels, false);
+
       function startupLabels(){
         colorWellLabels = document.querySelector("#colorWellLabels" + index);
         colorWellLabels.addEventListener("input", updateFirst, false);
@@ -236,10 +283,39 @@ $(document).ready(function(){
         }
       }
 
+      //Title
+      if(index === 0){
+        var colorWellTitle = document.createElement("input");
+        colorWellTitle.setAttribute("type", "color");
+        colorWellTitle.setAttribute("id", "colorWellTitle");
+
+        document.addEventListener("click", startupTitle, false);
+        function startupTitle(){
+          colorWellTitle = document.querySelector("#colorWellTitle");
+          colorWellTitle.addEventListener("input", updateTitle, false);
+          colorWellTitle.select();
+        }
+        function updateTitle(event) {
+          var title = document.querySelector(".chartTitle");
+          if(title){
+            title.style.color = event.target.value;
+          }
+        }
+      }
+
+
+
+      //Labels for Bar Color Picker
+      var colorLabel = document.createElement("label");
+      labelLabel.setAttribute("for", label);
+      var nameLabel = document.createTextNode(this.Label);
+
+
       //Labels for Label Color Picker
       var labelColorLabel = document.createElement("label");
       labelColorLabel.setAttribute("for", label);
       var nameLabelLabel = document.createTextNode(this.Label);
+
 
       //Dom insertion - Bar Colors
       $("#colors").append("<br>");
@@ -255,9 +331,16 @@ $(document).ready(function(){
       $("#colorLabels").append(colorWellLabels);
       $("#colorLabels").append("<br>");
 
+      //DOM insertion - Title Colors
+      $("#titleDiv").append("<br>");
+      $("#titleDiv").append(colorWellTitle);
+      $("#titleDiv").append("<br>");
+
     });
 
-    return element.append(table);
+    element.append(table);
+
+    return element;
 
     }
 
